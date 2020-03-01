fe33519e63ea653082de732c55f24bc619b1baf8
summmiii <xus057@newschool.edu>
2020-03-01 03:56:08 -0500

    animation

---
+++ exercises/scriptexample.js
@@ -0,0 +1,19 @@
+// const $description = document.querySelector("#container");
+// console.log($container);
+
+var $items = document.querySelectorAll(".item");
+
+console.log($items.length);
+// console.log($items(0));
+
+// use const or var or let
+
+// for (let i = 0; i < 8; let i++) {
+// $items[i].innerHTML = 'item 🎈';
+// }
+
+// $items.forEach(item => {
+//   item.innerHTML = "item🎈";
+// });
+
+//  const myArray = ['string', 42, {title: 'a description'}]
